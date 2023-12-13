f7d8ae48ead598be60a7d843f68df4eb57563c79
Zakaria Ismail <1112zakaria@gmail.com>
2023-12-13 17:02:20 -0500

    added max accuracy callback for LSTM training

--- LSTM.py
+++ LSTM.py
@@ -4,9 +4,17 @@
 import os
 from keras import models
 from keras import layers
+import tensorflow as tf
 from data_prep import trainX, trainY
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+MAX_ACCURACY = 0.99
+
+class MaxAccuracyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') is not None and logs.get('accuracy') > MAX_ACCURACY:
+            print(f"Reached max accuracy of {MAX_ACCURACY}. Ending training")
+            self.model.stop_training = True
 
 model = models.Sequential()
 model.add(layers.LSTM(128,
@@ -28,7 +36,7 @@ model.compile(
 )
 # model.summary()
 
-history = model.fit(trainX, trainY, batch_size=32, epochs=20, validation_split=0.1, verbose=2)
+history = model.fit(trainX, trainY, batch_size=32, validation_split=0.1, verbose=2, callbacks=[MaxAccuracyCallback])
 
 model.save(os.path.join('models', 'model.h5'))
 
