5102a729c2e88550eea0a094c6606ba2534db533
Ben Evensen <57432484+benevensen@users.noreply.github.com>
2024-02-06 18:20:22 -0500

    Update get_forcast.m

--- Ottawa_Canada/get_forcast.m
+++ Ottawa_Canada/get_forcast.m
@@ -5,8 +5,26 @@
 pyrunfile("forcast.py");
 
 %import the prediction function from forcast.py
-pyrun("from forcast import get_prediction");
+pyrun("from forcast import get_prediction, get_prediction_with_input");
 
-%store the get_prediction() result
-result = pyrun("result = get_prediction()", "result");
-disp(result)
+x = input('Are you using manual entry? (y/n) ', 's');
+
+%manually get the weater prediction
+if strcmp(x,'y')
+    temperature = input('Enter the temperature: ');
+    humidity = input('Enter the humidity: ');
+    precipitation = input('Enter the precipitation: ');
+    wind_speed = input('Enter the wind speed: ');
+    pressure = input('Enter the pressure: ');
+    result = pyrun("result = get_prediction_with_input(t, h, p, ws, pr)", "result", t=temperature, h=humidity, p=precipitation, ws=wind_speed, pr=pressure);
+
+%automatically get the weather prediction
+else
+    result = pyrun("result = get_prediction()", "result");
+end
+
+disp(result);
+
+
+
+
